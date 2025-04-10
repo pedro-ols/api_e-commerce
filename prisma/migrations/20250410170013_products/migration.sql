9c92cd23bbcd78ae10937f28dc2b3ce9eb111e1c
Pedro Santos <pedrosantos.tds2.senai@gmail.com>
2025-04-10 14:00:42 -0300

    feat: create prisma migration

---
+++ prisma/migrations/20250410170013_products/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
+    "category" TEXT NOT NULL,
+    "brand" TEXT NOT NULL,
+    "stock" INTEGER NOT NULL,
+    "imageUrl" TEXT,
+    "isActive" BOOLEAN,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
